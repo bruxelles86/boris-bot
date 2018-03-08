9d450c46b8ef8e0efd447ca23b362c25433ee86b
bruxelles86 <w_gant@yahoo.co.uk>
2018-03-08 21:11:23 +0000

    Sweet first step

---
+++ bot.js
@@ -0,0 +1,55 @@
+const twit = require('twit');
+const config = require('./config.js');
+const BORIS_HANDLE = '@BorisJohnson'
+const CHARACTER_LIMIT = 280
+const INTERVAL = 15 * 60 * 1000
+
+const Twitter = new twit(config);
+
+const quotes = [
+    "If we left the EU, we would end this sterile debate, and we would have to recognise that most of our problems are not caused by “Bwussels”, but by chronic British short-termism, inadequate management, sloth, low skills, a culture of easy gratification and underinvestment in both human and physical capital and infrastructure",
+    "Britain is a great nation, a global force for good. It is surely a boon for the world and for Europe that she should be intimately engaged in the EU. This is a market on our doorstep, ready for further exploitation by British firms: the membership fee seems rather small for all that access.",
+    "If we get to this campaign, I would be well up for trying to make the positive case for some of the good things that have come from the single market",
+    "Leaving would cause at least some business uncertainty, while embroiling the Government for several years in a fiddly process of negotiating new arrangements, so diverting energy from the real problems of this country",
+    "The choice is really quite simple. In favour of staying, it is in Britain’s geo-strategic interests to be pretty intimately engaged in the doings of a continent that has a grim 20th-century history, and whose agonies have caused millions of Britons to lose their lives.",
+    "I would vote to stay in the single market. I'm in favour of the single market. I want us to be able to trade freely with our European friends and partners.",
+    "I don't think it's as simple as yes, no, in out. Suppose Britain voted tomorrow to come out. What would actually happen? In real terms, what would happen is that the foreign office would immediately build a huge – well, the entire delegation would remain in Brussels. UKREP would remain there, we'd still have huge numbers of staff trying to monitor what was going on in the community, only we wouldn't be able to sit in the Council of Ministers. We wouldn't have any vote at all. Now I don't think that's actually a prospect that's likely to appeal. What you could do, is think of a new arrangement, new areas of the treaty that we didn't want to participate in any more. That is the where people are thinking now. So I don't think it is – I mean, with great respect to the sort of in-outers, I don't think it does boil down to such a simple question.",
+    "London is the most commercially important city in Europe, and it's the most populous city. It should be for the whole of the European continent what New York is to America. That's what it should be.",
+    "I’m probably about the only politician I know of who is actually willing to stand up and say that he’s pro-immigration.",
+    "There are tangible benefits to our membership of the European Union.",
+    "I believe in the free market of services and all those things."
+]
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sendTweet () {
+    let minimum = 0
+    let maximum = quotes.length -1
+    let tweetIndex = getRandomInt(minimum, maximum)
+    let tweet = quotes[tweetIndex]
+
+    if (tweet.length > CHARACTER_LIMIT - BORIS_HANDLE.length) {
+        tweet = tweet.substring(0, CHARACTER_LIMIT - BORIS_HANDLE.length - 1)
+    }
+
+    tweet = `${tweet} ${BORIS_HANDLE}`
+
+    console.log(`Tweeting: ${tweet}`)
+
+    Twitter.post('statuses/update', { status: tweet }, function(err, data, response) {
+        
+        if (!err) {
+            console.log('Success')
+        } else {
+            console.log(err)
+        }
+    })
+}
+
+function queueTweets () {
+    setInterval(sendTweet, INTERVAL)
+}
+
+queueTweets()
