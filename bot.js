a4fad10939f5acc29ea6c99c1e6a189ed25b2cc9
Will Gant <w_gant@yahoo.co.uk>
2018-03-22 21:46:44 +0000

    Edits down quotes so all fit in one tweet

--- bot.js
+++ bot.js
@@ -16,15 +16,15 @@ let UsingQuotes1 = true
 const Twitter = new twit(CONFIG);
 
 const quotes1 = [
-    "If we left the EU, we would end this sterile debate, and we would have to recognise that most of our problems are not caused by “Bwussels”, but by chronic British short-termism, inadequate management, sloth, low skills, a culture of easy gratification and underinvestment in both human and physical capital and infrastructure",
+    "If we left the EU, we would end this sterile debate, and we would have to recognise that most of our problems are not caused by “Bwussels”",
     "This is a market on our doorstep, ready for further exploitation by British firms: the membership fee seems rather small for all that access.",
     "Britain is a great nation, a global force for good. It is surely a boon for the world and for Europe that she should be intimately engaged in the EU.",
     "If we get to this campaign, I would be well up for trying to make the positive case for some of the good things that have come from the single market",
     "Leaving would cause at least some business uncertainty, while embroiling the Government for several years in a fiddly process of negotiating new arrangements, so diverting energy from the real problems of this country",
-    "The choice is really quite simple. In favour of staying, it is in Britain’s geo-strategic interests to be pretty intimately engaged in the doings of a continent that has a grim 20th-century history, and whose agonies have caused millions of Britons to lose their lives.",
+    "It is in Britain’s geo-strategic interests to be pretty intimately engaged in the doings of a continent that has a grim 20th-century history, and whose agonies have caused millions of Britons to lose their lives.",
     "I would vote to stay in the single market. I'm in favour of the single market. I want us to be able to trade freely with our European friends and partners.",
     "We can’t leave Europe. We’re part of the European Continent. What is the English Channel? It’s a primeval river that got slightly too big",
-    "Suppose Britain voted tomorrow to come out. What would actually happen? In real terms, what would happen is that the foreign office would immediately build a huge – well, the entire delegation would remain in Brussels. UKREP would remain there, we'd still have huge numbers of staff trying to monitor what was going on in the community, only we wouldn't be able to sit in the Council of Ministers. We wouldn't have any vote at all. Now I don't think that's actually a prospect that's likely to appeal. With great respect to the sort of in-outers, I don't think it does boil down to such a simple question.",
+    "Suppose Britain voted tomorrow to come out. We'd still have huge numbers of staff trying to monitor what was going, only we wouldn't have any vote at all. Now I don't think that's actually a prospect that's likely to appeal.",
     "London is the most commercially important city in Europe, and it's the most populous city. It should be for the whole of the European continent what New York is to America. That's what it should be.",
     "I’m probably about the only politician I know of who is actually willing to stand up and say that he’s pro-immigration.",
     "The trouble is, I am not an ‘outer’.",
